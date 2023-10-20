8ba6d7e574a266408d567ddafd6122d7ff66ede7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-10-20 12:14:23 -0600

    commit and test

--- assets/js/assist.js
+++ assets/js/assist.js
@@ -1,6 +1,23 @@
-function checkSpecialWords(){
-    switch (word){
+function checkSpecialWords() {
+    switch (word) {
         case "Inicio":
             document.getElementById('enter').innerHTML += ("\nFin");
+            break;
+        default:
+            break;
     }
+}
+
+function checkLastWord(input) {
+    // Obtén el valor actual del campo de entrada
+    const text = input.value;
+
+    // Divide el texto en palabras utilizando un espacio en blanco como separador
+    const words = text.split(' ');
+
+    // Obtiene la última palabra completa escrita
+    const lastWord = words[words.length - 1];
+
+    // Muestra la última palabra en un elemento HTML
+    document.getElementById('lastWord').textContent = `Última palabra: ${lastWord}`;
 }
