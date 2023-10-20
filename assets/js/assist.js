898da48de710118dfdc07e629cc3c9a77dadb6df
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-10-20 12:36:18 -0600

    carga de funciones modificadas

--- assets/js/assist.js
+++ assets/js/assist.js
@@ -1,26 +1,56 @@
-function checkSpecialWords() {
+function checkSpecialWords(word) {
+    const textarea = document.getElementById('enter');
+
+    // Guarda la posición actual del cursor
+    const inicio = textarea.selectionStart;
+    const fin = textarea.selectionEnd;
+
     switch (word) {
         case "Inicio":
-            document.getElementById('enter').innerHTML += ("\nFin");
+            textarea.value += ("\nFin");
             break;
         default:
             break;
     }
+    textarea.setSelectionRange(inicio, fin);
 }
 
 function checkLastWord(input) {
     // Obtén el valor actual del campo de entrada
+    var lastWord;
     const text = input.value;
 
     // Divide el texto en palabras utilizando un espacio en blanco como separador
     const words = text.split(' ');
 
     // Obtiene la última palabra completa escrita
-    const lastWord = words[words.length - 1];
+    lastWord = words[words.length - 1];
     if (lastWord == " ") {
-        const lastWord = words[words.length - 2];
+        lastWord = words[words.length - 2];
     }
+
+    checkSpecialWords(lastWord);
 
     // Muestra la última palabra en un elemento HTML
     document.getElementById('lastWord').textContent = `Última palabra: ${lastWord}`;
+}
+
+function agregarSaltoDeLinea() {
+    const textarea = document.getElementById('miTextarea');
+
+    // Guarda la posición actual del cursor
+    const inicio = textarea.selectionStart;
+    const fin = textarea.selectionEnd;
+
+    // Obtiene el contenido actual del textarea
+    const contenido = textarea.value;
+
+    // Agrega un salto de línea al final del contenido
+    const nuevoContenido = contenido + '\n';
+
+    // Actualiza el contenido del textarea
+    textarea.value = nuevoContenido;
+
+    // Restaura la posición del cursor al final
+    textarea.setSelectionRange(inicio, fin);
 }
